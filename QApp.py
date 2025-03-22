b357c51fa09d8a718c5b5be6e0a246589a8c6c05
daviddoust1 <david.doust@outlook.com>
2025-03-22 20:30:41 +0800

    Add files via upload

---
+++ QApp.py
@@ -0,0 +1,93 @@
+from PIL import Image
+import pyautogui
+import numpy as np
+import pandas as pd
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+# Streamlit app: To run, open a terminal and run:
+# streamlit run QApp.py
+# May need to press enter to get past the welcome message
+
+st.write('# QuantApp')
+st.write(' ')
+
+canvas_result = st_canvas(
+    stroke_color='#ed13e2',
+    # rgb 237, 19, 226
+    stroke_width=4,
+    height=500,
+    width=600,
+    background_image=Image.open('C:/Users/David/TriangleText.png'), 
+    drawing_mode='point',
+    point_display_radius=4)
+
+complete = st.checkbox(label='Complete', value=False)
+
+if complete:
+    # if canvas_result.image_data is not None:
+    #     st.image(canvas_result.image_data)
+    if canvas_result.json_data is not None:
+        objects = pd.json_normalize(canvas_result.json_data["objects"])
+        for col in objects.select_dtypes(include=["object"]).columns:
+            objects[col] = objects[col].astype("str")
+        # st.dataframe(objects)
+        # Configure the below coords by placing a point on each corner
+        # and looking at the coords in objects dataframe
+        top = [291,52]
+        left = [81,440]
+        right = [500,438]
+        selection = [int(objects['left'][0]), int(objects['top'][0])]
+
+        # Will need to fix these a bit - max_dist changes with the position of the point
+        max_dist = ((top[0] - left[0])**2 + (top[1] - left[1])**2)**0.5
+        top_score = 1-((((selection[0] - top[0])**2 + (selection[1] - top[1])**2)**0.5)/max_dist)
+        left_score = 1-((((selection[0] - left[0])**2 + (selection[1] - left[1])**2)**0.5)/max_dist)
+        right_score = 1-((((selection[0] - right[0])**2 + (selection[1] - right[1])**2)**0.5)/max_dist)
+
+        st.write("things score: " + str(top_score))
+        st.write("stuff and also things score: " + str(left_score))
+        st.write("things and stuff score: " + str(right_score))
+        
+
+
+
+
+    # # Blue top, green left, purple right
+    # # Make sure you actually have the triangle on screen
+    # img = pyautogui.screenshot()
+    # st.image(img)
+    # arr = np.array(img)
+    
+    # # Works ok for now
+    # # Will need to make these more robust later using proximity to yellow pixels
+    # blue = np.where((arr[:,:,0]==77) & (arr[:,:,1]==190) & (arr[:,:,2]==239))
+    # green = np.where((arr[:,:,0]==99) & (arr[:,:,1]==200) & (arr[:,:,2]==128))
+    # purple = np.where((arr[:,:,0]==163) & (arr[:,:,1]==73) & (arr[:,:,2]==164))
+    # # yellow = np.where((arr[:,:,0]==255) & (arr[:,:,1]==242) & (arr[:,:,2]==0))
+    # red = np.where((arr[:,:,0]==237) & (arr[:,:,1]==19) & (arr[:,:,2]==226))
+    
+    # st.write(blue)
+    # st.write(green)
+    # st.write(purple)
+    # st.write(red)
+
+    # for colour in [blue, green, purple]:
+    #     colour = [colour[0][0],colour[1][0]]
+
+    # max_dist = ((top[0] - left[0])**2 + (top[1] - left[1])**2)**0.5
+    # top_score = 1/((((selection[0] - top[0])**2 + (selection[1] - top[1])**2)**0.5)/max_dist)
+    # left_score = 1/((((selection[0] - left[0])**2 + (selection[1] - left[1])**2)**0.5)/max_dist)
+    # right_score = 1/((((selection[0] - right[0])**2 + (selection[1] - right[1])**2)**0.5)/max_dist)
+
+    # print(blue_score)
+    # print(green_score)
+    # print(purple_score)
+
+    # df = pd.DataFrame(data={'Top': blue_score,
+    #                         'Left': green_score,
+    #                         'Right': purple_score})
+
+#  st.dataframe(data=df)
+# st.write(red)
+
