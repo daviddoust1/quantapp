1c302dbca752ca38de6aa113ca9594a22380509e
daviddoust1 <david.doust@outlook.com>
2025-03-23 14:21:03 +0800

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -5,8 +5,8 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import urllib.request
 
-urllib.request.urlretrieve('https://raw.githubusercontent.com/daviddoust1/quantapp/refs/heads/main/TriangleText.png', 'TriangleText.png')
-a = Image.open('TriangleText.png')
+# urllib.request.urlretrieve('https://raw.githubusercontent.com/daviddoust1/quantapp/refs/heads/main/TriangleText.png', 'TriangleText.png')
+# a = Image.open('TriangleText.png')
 
 # Streamlit app: To run, open a terminal and run:
 # streamlit run QApp.py
@@ -21,7 +21,7 @@ canvas_result = st_canvas(
     stroke_width=4,
     height=500,
     width=600,
-    background_image=a, 
+    background_image=Image.open('TriangleText.png'), 
     drawing_mode='point',
     point_display_radius=4)
 
